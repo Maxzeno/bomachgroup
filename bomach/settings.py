5bc12c0ff96905cdbb021afe80645f74e912bc29
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-06-23 16:28:53 +0100

    removed pillow ck editor backend because of bug

--- bomach/settings.py
+++ bomach/settings.py
@@ -193,7 +193,7 @@ else:
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 # if not TRY_LOCAL_STORAGE:
-CKEDITOR_IMAGE_BACKEND = "pillow"
+# CKEDITOR_IMAGE_BACKEND = "pillow"
 CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js' 
 
 
